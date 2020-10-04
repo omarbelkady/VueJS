759604ffe12f13151aa62952bcd32abbee3614ff
Omar <omar.belkady@g.austincc.edu>
2020-10-04 19:41:15 +0100

    2526 56837 727225

---
+++ Vue_Instance/app/src/app.js
@@ -0,0 +1,9 @@
+//A vue instance takes control either of a whole application or part of an application
+//Linking the div id to here
+//Creating data within the vue instance of type object
+new Vue({
+	el: '#vue-app',
+	data: {
+		name: "Pasc Lover"
+	}
+});
