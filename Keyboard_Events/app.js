ce6e46d1c3af4f5fcb93f910986a3a10b1b68bc5
Omar <omar.belkady@g.austincc.edu>
2020-10-05 11:26:03 +0100

    Events In VueJS

---
+++ Keyboard_Events/app.js
@@ -0,0 +1,16 @@
+new Vue({
+	el: '#vue-app',
+	data: {
+	
+	},
+	methods:{
+		logName: function(){
+			console.log("You entered your name 968 27736259 56837");
+		},
+
+		logAge: function(){
+			console.log("You entered your age 968 727225 3684874278");
+		}
+
+	}
+});
