d45a6fa7cce0b11a64e24ab0d12f7e3054e1208b
Omar Belkady <31806568+omarbelkady@users.noreply.github.com>
2020-11-05 02:34:14 +0000

    Input Bindings in VueJS

---
+++ Input_Binding/vuejsproj/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+      <div>
+          <add-blog></add-blog>
+      </div>
+
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue';
+export default {
+  //line 15 is how I register a component to able to use it
+  //left is the name you would like to register it by and the right value is the value you want to map it to
+  //line 3 is how I nest a component
+  components:{
+    'add-blog':addBlog
+  },
+  data(){
+    return {
+       
+    }
+  },
+  methods:{
+   
+    }
+  }
+</script>
+
+<style>
+body{
+  margin: 0;
+  font-family: 'Nunito Semibold';
+}
+</style>
---
+++ Input_Binding/vuejsproj/src/components/addBlog.vue
@@ -0,0 +1,79 @@
+<template>
+      <div id="add-blog">
+        <h2>Add A New CS-375FB Post</h2>
+        <form>
+            <label>Blog Title</label>
+            <input type="text" v-model.lazy="blog.title" required />
+            <label>Blog Content</label>
+            <textarea v-model.lazy="blog.content"></textarea>
+        </form>
+        <div id="preview">
+          <h3>Preview The CS375-32's Blog</h3>
+          <p>Blog Title:{{ blog.title }}</p>
+          <p>Blog Content: </p>
+          <p>{{ blog.content }} </p>
+        </div>
+     
+      </div>
+
+</template>
+
+<script>
+
+export default {
+  /*I want to have two fields in my form one for the title 
+  and a text area for the blog content and make sure the user
+  enter data in the text field
+  I also want the user to be able to preview his post
+
+  now in the input type and text area notice how they both share the same
+  object (blog). This is done so that we store the different related fields 
+  into one blog object. In VueJS terms keep all the components together 
+
+  I want to be able to display the updated input until I move on to the next text
+  input and not on the go using what's called an input modifier using the keyword
+  v-model.lazy
+  */
+  data(){
+    return {
+       blog:{
+        title: "",
+        content: ""
+      }
+    }
+  },
+  methods:{
+   
+    }
+  }
+</script>
+
+<style>
+#add-blog *{
+  box-sizing: border-box;
+}
+
+#add-blog{
+  margin: 20px auto; 
+}
+
+label{
+  display: block;
+  margin: 20px 0 10px;
+}
+
+input[type="text"],textarea{
+  display: block;
+  width: 100%;
+}
+
+#preview{
+  padding: 10px 20px;
+  border: 1px dotted #ccc;
+  margin: 30px 0;
+}
+
+h3{
+  margin-top: 10px;
+}
+</style>
